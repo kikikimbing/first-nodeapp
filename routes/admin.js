9dc3c64561af3409a6cf8be462de1824ea51679e
anantatama <anantatama96@gmail.com>
2019-08-09 22:55:00 +0700

    Adding new functionality and routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,19 @@
 const path = require('path');
 const express = require('express');
 
-// All controllers using ../controllers/produst.js
+// All controllers using ../controllers/product.js
 // Used to simplify app routing
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // '/admin/add-product' => method GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getProducts);
 
 // ''/admin/add-product' => method POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 // exporting router, so it can be used by another file
 module.exports = router;
